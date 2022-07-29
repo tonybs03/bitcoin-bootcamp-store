e6bd47e7b64dbc7f9a1618ef9312540e68c10f12
pletzjd <j.pletzer17@gmail.com>
2022-07-29 01:09:15 -0400

    added additional user information to the dashboard. Added form for updating user info (no logic currently for submitting

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
 function OrderHistory() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { data } = useQuery(QUERY_USER);
   let user;
 
@@ -12,6 +17,8 @@ function OrderHistory() {
     user = data.user;
     console.log(user)
   }
+
+
 
   return (
     <>
@@ -24,15 +31,52 @@ function OrderHistory() {
               Welcome {user.firstName} {user.lastName}
             </h2>
 
+            <div className='flex-row'>
+              <div>
+                <h3>{user.firstName} {user.lastName} Info</h3>
+                <h4>You have: {user.bitcoin} ฿</h4>
+                <h4>You have purchased: {user.orders[0].products.length} chocolate so far</h4>
+                <h4>You are at: # on the leaderboard</h4>
+              </div>
+
+              <div>
+                <h3>Update Information</h3>
+                <form>
+                  <input
+                    placeholder='first name'
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}
+                    ></input>
+                  <input
+                    placeholder='last name'
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
+                    ></input>
+                  <input
+                    placeholder='email'
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    ></input>
+                  <input
+                    placeholder='password'
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    ></input>
+                  <div>
+                    <button type='submit'>Submit</button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+
             <h3> Order History </h3>
+
             {user.orders.map((order) => (
               <div key={order._id} className="my-2">
                 <h5>
                   {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
                 </h5>
-                <div>
-                  <h4>You have: {user.bitcoin} ฿</h4>
-                </div>
                 <div className="flex-row">
                   {order.products.map(({ _id, image, name, price }, index) => (
                     <div key={index} className="card px-1 py-1">
@@ -41,7 +85,7 @@ function OrderHistory() {
                         <p>{name}</p>
                       </Link>
                       <div>
-                        <span>${price}</span>
+                        <span>{price} ฿</span>
                       </div>
                     </div>
                   ))}
