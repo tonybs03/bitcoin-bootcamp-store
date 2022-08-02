a58fc6e3cad05b8aa337366c2fca8ba95a2b9869
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-08-02 11:48:23 -0400

    Deployment attempt 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JKT:mongodb@bitcoin-bootcamp-store.tszgt1n.mongodb.net/bitcoin-bootcamp-store?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
