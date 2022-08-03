9e3924025c1d586c75f269c3ad81e1ef29b1539c
Tony J.J Fan <62399532+tonybs03@users.noreply.github.com>
2022-08-03 10:20:53 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JKT:mongodb@bitcoin-bootcamp-store.tszgt1n.mongodb.net/bitcoin-bootcamp-store?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bootcamp-store', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JKT:mongodb@bitcoin-bootcamp-store.tszgt1n.mongodb.net/bitcoin-bootcamp-store?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
 });
+
 
 module.exports = mongoose.connection;
